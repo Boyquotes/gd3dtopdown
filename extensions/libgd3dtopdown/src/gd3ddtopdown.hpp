08c14f43b11c6317724c031e99fe0c075d963db5
unknown <jonquin@gmail.com>
2022-10-06 01:04:41 +0200

    Base character3d movement Implemented( input errors)

--- extensions/libgd3dtopdown/src/gd3ddtopdown.hpp
+++ extensions/libgd3dtopdown/src/gd3ddtopdown.hpp
@@ -8,25 +8,41 @@
 
 #include <godot_cpp/core/binder_common.hpp>
 
+#include <godot_cpp/classes/project_settings.hpp>
+
 #include <godot_cpp/classes/input.hpp>
-#include <godot_cpp/classes/input_event_mouse_motion.hpp>
+
 #include <godot_cpp/classes/input_event.hpp>
+#include <godot_cpp/classes/input_event_mouse_motion.hpp>
+
+#include <godot_cpp/classes/input_map.hpp>
+
 
 using namespace godot;
 
 class GD3Dtopdown : public CharacterBody3D {
     GDCLASS(GD3Dtopdown, CharacterBody3D);
 
+    GD3Dtopdown();
     ~GD3Dtopdown();
 protected:
     static void _bind_methods();
+   
 
+    
 private:
     bool initialized;
     
     Input* input;
+    ProjectSettings* p_settings;
 
-    Vector3 player_position;
+    float mouse_sensitivity = 0.2f;
+
+    float player_speed = 5.0f;
+    float player_jump_velocity = 4.5f;
+
+    float gravity = 9.8f;
+
     Vector3 lookat_position;
 
     Transform3D camera_transform = Transform3D();
@@ -37,22 +53,28 @@ public:
     virtual bool _initialize() ;
     virtual void _uninitialize() ;
 
+    void set_mouse_sensitivity(const float sen);
+    void set_player_speed(const float spd);
+    void set_player_jump_velocity(const float vel);
 
-    void set_player_position(const Vector3 &pos);
     void set_lookat_position(const Vector3 &pos);
     void set_camera_transform(const Transform3D &transform);
-    Vector3 get_player_position() const;
+
+    float get_mouse_sensitivity() const;
+    float get_player_speed() const;
+    float get_player_jump_velocity() const;
+   
     Vector3 get_lookat_position() const;
     Transform3D get_camera_transform() const;
 
- //   void input(const Ref<InputEvent>& p_event) override;
-
-    Vector3 move_player_with_camera_direction(const Vector2& dir);
-    
+    //Vector3 move_player_with_camera_direction(const Vector2& dir);
 
     void _ready() override;
+
+    void _physics_process(double delta) override;
    
    // void _input(Variant p_event);
     void _input(const Ref<InputEvent>& p_event) override;
+    void _input_handle(const Ref<InputEvent>& p_event);
    
 };
--- extensions/libgd3dtopdown/src/gd3dtopdown.cpp
+++ extensions/libgd3dtopdown/src/gd3dtopdown.cpp
@@ -1,6 +1,11 @@
 #include "gd3ddtopdown.hpp"
 
-#include <godot_cpp/classes/ref.hpp>
+//#include <godot_cpp/classes/ref.hpp>
+GD3Dtopdown::GD3Dtopdown()
+{
+    initialized = false;
+
+}
 
 GD3Dtopdown::~GD3Dtopdown()
 {
@@ -9,12 +14,19 @@ GD3Dtopdown::~GD3Dtopdown()
 
 void GD3Dtopdown::_bind_methods()
 {
-   // ClassDB::bind_method(D_METHOD("_input", "event"), &GD3Dtopdown::_input);
-
     
-    ClassDB::bind_method(D_METHOD("get_player_position"), &GD3Dtopdown::get_player_position);
-    ClassDB::bind_method(D_METHOD("set_player_position", "player_position"), &GD3Dtopdown::set_player_position);
-    ADD_PROPERTY(PropertyInfo(Variant::VECTOR3, "player_position"), "set_player_position", "get_player_position");
+    ClassDB::bind_method(D_METHOD("get_mouse_sensitivity"), &GD3Dtopdown::get_mouse_sensitivity);
+    ClassDB::bind_method(D_METHOD("set_mouse_sensitivity", "mouse_sensitivity"), &GD3Dtopdown::set_mouse_sensitivity);
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "mouse_sensitivity"), "set_mouse_sensitivity", "get_mouse_sensitivity");
+
+    ClassDB::bind_method(D_METHOD("get_player_speed"), &GD3Dtopdown::get_player_speed);
+    ClassDB::bind_method(D_METHOD("set_player_speed", "player_speed"), &GD3Dtopdown::set_player_speed);
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "player_speed"), "set_player_speed", "get_player_speed");
+
+    ClassDB::bind_method(D_METHOD("get_player_jump_velocity"), &GD3Dtopdown::get_player_jump_velocity);
+    ClassDB::bind_method(D_METHOD("set_player_jump_velocity", "player_jump_velocity"), &GD3Dtopdown::set_player_jump_velocity);
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "player_jump_velocity"), "set_player_jump_velocity", "get_player_jump_velocity");
+
 
     ClassDB::bind_method(D_METHOD("get_lookat_position"), &GD3Dtopdown::get_lookat_position);
     ClassDB::bind_method(D_METHOD("set_lookat_position", "lookat_position"), &GD3Dtopdown::set_lookat_position);
@@ -23,14 +35,23 @@ void GD3Dtopdown::_bind_methods()
     ClassDB::bind_method(D_METHOD("get_camera_transform"), &GD3Dtopdown::get_camera_transform);
     ClassDB::bind_method(D_METHOD("set_camera_transform", "camera_transform"), &GD3Dtopdown::set_camera_transform);
  
-    ClassDB::bind_method(D_METHOD("move_player_with_camera_direction"), &GD3Dtopdown::move_player_with_camera_direction);
-
+  //  ClassDB::bind_method(D_METHOD("move_player_with_camera_direction"), &GD3Dtopdown::move_player_with_camera_direction);
+    //Register of input_handle
+    ClassDB::bind_method(D_METHOD("_input_handle"), &GD3Dtopdown::_input_handle);
     
 }
 
-void GD3Dtopdown::set_player_position(const Vector3& pos)
+void GD3Dtopdown::set_mouse_sensitivity(const float sen)
 {
-    player_position = pos;
+    mouse_sensitivity = sen;
+}
+void GD3Dtopdown::set_player_speed(const float spd)
+{
+    player_speed = spd;
+}
+void GD3Dtopdown::set_player_jump_velocity(const float vel)
+{
+    player_jump_velocity = vel;
 }
 void GD3Dtopdown::set_lookat_position(const Vector3& pos)
 {
@@ -42,10 +63,17 @@ void GD3Dtopdown::set_camera_transform(const Transform3D& transform)
     
     camera_transform = transform;
 }
-Vector3 GD3Dtopdown::get_player_position() const
+float GD3Dtopdown::get_mouse_sensitivity() const
 {
-    
-    return player_position;
+    return mouse_sensitivity;
+}
+float GD3Dtopdown::get_player_speed() const
+{
+    return player_speed;
+}
+float GD3Dtopdown::get_player_jump_velocity() const
+{
+    return player_jump_velocity;
 }
 Vector3 GD3Dtopdown::get_lookat_position() const
 {
@@ -64,12 +92,32 @@ bool GD3Dtopdown::_initialize()
 {
     if (!initialized)
     {
+        p_settings = ProjectSettings::get_singleton();
         input = Input::get_singleton();
+
+        if (p_settings == nullptr)
+        {
+            _uninitialize();
+            ERR_FAIL_V_MSG(false, "Couldn't obtain ProjectSettings singleton, didnt initialize");
+        }
         if (input == nullptr)
         {
-            ERR_FAIL_V_MSG(false,"Couldn't obtain Input singleton, didnt initialize");
+            _uninitialize();
+            ERR_FAIL_V_MSG(false, "Couldn't obtain Input singleton, didnt initialize");
         }
         
+        Variant grav = p_settings->get_setting("physics/3d/default_gravity");
+        if (grav.get_type() == Variant::INT || grav.get_type() == Variant::FLOAT)
+        {
+            gravity = float(grav);
+           
+        }else
+        {
+            
+            WARN_PRINT("Could not obtain value for gravity from project settings, defaulting to 9.8");
+        }
+        gravity = 9.8f;
+        WARN_PRINT("Could not obtain value for gravity from project settings, defaulting to 9.8");
         initialized = true;
         WARN_PRINT("Initialized GD3D");
     }
@@ -79,6 +127,7 @@ void GD3Dtopdown::_uninitialize()
 {
     if (initialized)
     {
+        p_settings = nullptr;
         input = nullptr;
         initialized = false;
     }
@@ -89,6 +138,67 @@ void GD3Dtopdown::_ready()
 {
     _initialize();
     WARN_PRINT("Ready function executed");
+}
+
+void GD3Dtopdown::_input(const Ref<InputEvent>& p_event)
+{
+    //Not being captured void GD3Dtopdown::_input(Variant p_event) neither
+    // use _input handle instead (look at gd3dtopdown_godot/scripts/gd3dtopdown.gd as a temporary workaround
+    WARN_PRINT_ONCE("InputFunctionCalledRef");
+}
+void GD3Dtopdown::_input_handle(const Ref<InputEvent>& p_event)
+{
+    //Temporary workaround for handling input events
+
+    Ref<InputEventMouseMotion> m = p_event;
+    if (m.is_valid())
+    {
+        Vector2 motion = m->get_relative();
+        rotate(Vector3(0, 1, 0), Math::deg2rad(motion.x) * mouse_sensitivity);
+    }
+    return;
+}
+
+void GD3Dtopdown::_physics_process(double delta)
+{
+    WARN_PRINT_ONCE("Physics process running");
+    Vector3 vel = get_velocity();
+    bool floored = is_on_floor();
+    float speed = player_speed;
+
+    if (!floored)
+    {
+        vel.y -= gravity * delta;
+    }
+    
+    if (floored && input->is_action_pressed("jump"))
+    {
+        WARN_PRINT("Jump");
+        vel.y = player_jump_velocity;
+    }
+    
+    if (input->is_action_pressed("sprint"))
+    {
+        speed *= 2.0f;
+    }
+   
+    Vector2 input_dir = input->get_vector("move_left", "move_right", "move_forward", "move_back");
+
+    Vector3 direction = (get_transform().basis.xform(Vector3(input_dir.x, 0, input_dir.y))).normalized();
+   
+    if(direction.is_zero_approx())
+    {
+        vel.x = Math::move_toward(vel.x, 0, speed);
+        vel.z = Math::move_toward(vel.z, 0, speed);
+    }
+    else
+    {
+        vel.x = direction.x * speed;
+        vel.z = direction.z * speed;
+    }
+   
+    set_velocity(vel);
+   
 }
 /*void GD3Dtopdown::_input(Variant p_event)
 {
@@ -101,23 +211,3 @@ void GD3Dtopdown::_ready()
 
     return;
 }*/
-void GD3Dtopdown::_input(const Ref<InputEvent>& p_event)
-{
-    WARN_PRINT("InputFunctionCalledRef");
-    Ref<InputEventMouseMotion> m = p_event;
-    if (m.is_valid())
-    {
-        WARN_PRINT("moving");
-    }
-
-    return;
-}
-
-Vector3 GD3Dtopdown::move_player_with_camera_direction(const Vector2& inputvector)
-{
-   // Forward vector
-    -(camera_transform.basis.get_axis(2));
-
-    return -camera_transform.basis.get_axis(2);
-    move_and_slide();
-}
